782ad36206462d5f49eaca9bdcb997a7ce2eeb5d
Ben <ben@Computer-1.(none)>
2013-06-17 23:05:43 -0400

    3th

--- google-drive-panel/usr/share/google-drive-panel/google-drive-panel.py
+++ google-drive-panel/usr/share/google-drive-panel/google-drive-panel.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 import os, sys
 
-def setup():
-	print 'Setting up, may take a while'
-	os.system('/usr/share/google-drive-panel/setup.py > /dev/null &')
-	exit()
 def help():
 	print('usage:')
 	print('google-drive-panel [--setup]')
 	print('--setup  enters setup')
 	exit()
+
+def setup():
+	print 'Setting up, may take a while'
+	os.system('/usr/share/google-drive-panel/setup.py > /dev/null &')
+	exit()
+
 def start():
 	os.system('/usr/share/google-drive-panel/main.py > /dev/null & /usr/share/google-drive-panel/time_update.py > /dev/null &')
 	print 'Google Drive Panel Started! You can close this terminal now'
--- google-drive-panel/usr/share/google-drive-panel/setup_two.py
+++ google-drive-panel/usr/share/google-drive-panel/setup_two.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 import os
+
+#Check for root
 if os.geteuid() != 0:
 	print 'You do not have root privileges'
 	os.system('gksudo python "' + os.path.abspath( __file__ ) + '"')
 	exit()
+
 os.system('cd /usr/share/google-drive && sudo chmod 777 drive_dir')
 os.system('add-apt-repository -y ppa:nilarimogard/webupd8')
 os.system('add-apt-repository -y ppa:webupd8team/y-ppa-manager')
